f90c6828ad777fa23e3d31411535a8d06cc2b029
John de Kroon <j.de.kroon.jdk@gmail.com>
2015-06-03 20:09:32 +0200

    Refactoring of existing code\

---
+++ NN1/Encoder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN1
+{
+    //class containing all encoding methods
+    public static class Encoder
+    {
+        public static string EffectsEncoding(int index, int N)
+        {
+            if (N==2)
+            {
+                if (index == 0) return "-1";
+                else if (index == 1) return "1";
+            }
+
+            int[] values = new int[N - 1];
+            if (index == N-1) //last item is all -1s
+            {
+                for (int i = 0; i<values.Length; i++)
+                {
+                    values[i] = -1;
+                }
+            }
+            else
+            {
+                values[index] = 1; //0 values are already there
+            }
+            string s = values[0].ToString();
+            for (int i =0; i< values.Length; i++)
+            {
+                s += "," + values[i];
+            }
+            return s;
+        }
+
+        public static string DummyEncoding(int index, int N)
+        {
+            int[] values = new int[N];
+            values[index] = 1;
+            string s = values[0].ToString();
+            for (int i = 0; i < values.Length; i++)
+            {
+                s += "," + values[i];
+            }
+            return s;
+
+        }
+    }
+}
---
+++ NN1/FileTransformer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NN1
+{
+    static class FileTransformer
+    {
+        static void EncodeFile(string originalFile, string encodedFile, int column, string encodingType)
+        {
+            //encodingType: "effects" or "Dummy"
+            FileStream ifs = new FileStream(originalFile, FileMode.Open);
+            StreamReader sr = new StreamReader(ifs);
+            string line = "";
+            string[] tokens = null;
+
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            int itemNum = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                tokens = line.Split(',');
+                if (d.ContainsKey(tokens[column])==false)
+                {
+                    d.Add(tokens[column], itemNum++);
+                }
+            }
+            sr.Close();
+            ifs.Close();
+
+            int N = d.Count;
+
+            ifs = new FileStream(originalFile, FileMode.Open);
+            sr = new StreamReader(ifs);
+
+            FileStream ofs = new FileStream(encodedFile, FileMode.Create);
+            StreamWriter sw = new StreamWriter(ofs);
+            string s = null;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                s = "";
+                tokens = line.Split(',');
+
+                for (int i = 0; i<tokens.Length; i++)
+                {
+                    if (i==column)
+                    {
+                        int index = d[tokens[i]];
+                        if (encodingType == "effects")
+                        {
+                            s += Encoder.EffectsEncoding(index, N);
+                        }
+                        else if (encodingType == "dummy")
+                        {
+                            s += Encoder.DummyEncoding(index, N);
+                        }
+                    }
+                    else
+                    {
+                        s += tokens[i] + ",";
+                    }
+                }
+                s.Remove(s.Length - 1);
+                sw.WriteLine(s);
+            }
+            sw.Close(); ofs.Close();
+            sr.Close(); ifs.Close();
+        }
+    }
+}
